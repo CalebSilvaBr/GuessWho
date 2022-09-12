dd5df002a25eb445574df49590190addcf461a19
CalebSilvaBr <calebis.eu@gmail.com>
2022-09-12 15:47:03 -0300

    gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,16 @@
 // node.js Packages / Dependencies
-const gulp          = require('gulp');
-const sass          = require('gulp-sass');
-const uglify        = require('gulp-uglify');
-const rename        = require('gulp-rename');
-const concat        = require('gulp-concat');
-const cleanCSS      = require('gulp-clean-css');
-const imageMin      = require('gulp-imagemin');
-const pngQuint      = require('imagemin-pngquant'); 
+import { task, src as _src, dest, series, watch } from 'gulp';
+import sass, { logError } from 'gulp-sass';
+import uglify from 'gulp-uglify';
+import rename from 'gulp-rename';
+import concat from 'gulp-concat';
+import cleanCSS from 'gulp-clean-css';
+import imageMin, { gifsicle, jpegtran, optipng, svgo } from 'gulp-imagemin';
+import pngQuint from 'imagemin-pngquant'; 
 const browserSync   = require('browser-sync').create();
-const autoprefixer  = require('gulp-autoprefixer');
-const jpgRecompress = require('imagemin-jpeg-recompress'); 
-const clean         = require('gulp-clean');
+import autoprefixer from 'gulp-autoprefixer';
+import jpgRecompress from 'imagemin-jpeg-recompress'; 
+import clean from 'gulp-clean';
 
 
 // Paths
@@ -37,71 +37,71 @@ var paths = {
 }
 
 // Compile SCSS
-gulp.task('sass', function() {
-    return gulp.src(paths.src.scss)
-    .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError)) 
+task('sass', function() {
+    return _src(paths.src.scss)
+    .pipe(sass({outputStyle: 'expanded'}).on('error', logError)) 
     .pipe(autoprefixer())
-    .pipe(gulp.dest(paths.src.root + '/css'))
+    .pipe(dest(paths.src.root + '/css'))
     .pipe(browserSync.stream());
 });
 
 // Minify + Combine CSS
-gulp.task('css', function() {
-    return gulp.src(paths.src.css)
+task('css', function() {
+    return _src(paths.src.css)
     .pipe(cleanCSS({compatibility: 'ie8'}))
     .pipe(concat('leadmark.css'))
     .pipe(rename({ suffix: '.min' }))
-    .pipe(gulp.dest(paths.dist.css))
+    .pipe(dest(paths.dist.css))
 });
 
 // Minify + Combine JS
-gulp.task('js', function() {
-    return gulp.src(paths.src.js)
+task('js', function() {
+    return _src(paths.src.js)
     .pipe(uglify())
     .pipe(concat('leadmark.js'))
     .pipe(rename({ suffix: '.min' }))
-    .pipe(gulp.dest(paths.dist.js))
+    .pipe(dest(paths.dist.js))
     .pipe(browserSync.stream());
 });
 
 // Compress (JPEG, PNG, GIF, SVG, JPG)
-gulp.task('img', function(){
-    return gulp.src(paths.src.imgs)
+task('img', function(){
+    return _src(paths.src.imgs)
     .pipe(imageMin([
-        imageMin.gifsicle(),
-        imageMin.jpegtran(),
-        imageMin.optipng(),
-        imageMin.svgo(),
+        gifsicle(),
+        jpegtran(),
+        optipng(),
+        svgo(),
         pngQuint(),
         jpgRecompress()
     ]))
-    .pipe(gulp.dest(paths.dist.imgs));
+    .pipe(dest(paths.dist.imgs));
 });
 
 // copy vendors to dist
-gulp.task('vendors', function(){
-    return gulp.src(paths.src.vendors)
-    .pipe(gulp.dest(paths.dist.vendors))
+task('vendors', function(){
+    return _src(paths.src.vendors)
+    .pipe(dest(paths.dist.vendors))
 });
 
 // clean dist
-gulp.task('clean', function () {
-    return gulp.src(paths.dist.root)
+task('clean', function () {
+    return _src(paths.dist.root)
         .pipe(clean());
 });
 
 // Prepare all assets for production
-gulp.task('build', gulp.series('sass', 'css', 'js', 'vendors', 'img'));
+task('build', series('sass', 'css', 'js', 'vendors', 'img'));
 
 
 // Watch (SASS, CSS, JS, and HTML) reload browser on change
-gulp.task('watch', function() {
+task('watch', function() {
     browserSync.init({
         server: {
             baseDir: paths.root.www
         } 
     })
-    gulp.watch(paths.src.scss, gulp.series('sass'));
-    gulp.watch(paths.src.js).on('change', browserSync.reload);
-    gulp.watch(paths.src.html).on('change', browserSync.reload);
+    watch(paths.src.scss, series('sass'));
+    watch(paths.src.js).on('change', browserSync.reload);
+    watch(paths.src.html).on('change', browserSync.reload);
 });
